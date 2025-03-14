2001fc45651739c48ba10cec84367ffbbd020019
Sir Harunzy <Harunzy55@gmail.com>
2025-03-14 14:34:17 +0530

    add compter random choice

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 function getComputerChoice(){
-    let randomNum = Math.random();
-    
+    let randomNum = Math.floor(Math.random()  * 3);
+    if (randomNum === 0){
+        console.log("Rock");
+    }
+    else if (randomNum === 1){
+        console.log("Paper");
+    }
+    else{
+        console.log("Scissors");
+    }
 }
+
+
+getComputerChoice();
